b8f200628acc4ea9a8ca5c273340ad973bd762ff
Shabib Hyder Gazi <shabibgazi@Shabibs-MacBook-Air.local>
2026-05-16 16:09:25 -0400

    feat: added Grafana and Prometheus monitoring dashboard

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.routes import router
 from app.database import init_db
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Feature Store")
+
+# Add Prometheus metrics
+Instrumentator().instrument(app).expose(app)
 
 @app.on_event("startup")
 def startup_event():
